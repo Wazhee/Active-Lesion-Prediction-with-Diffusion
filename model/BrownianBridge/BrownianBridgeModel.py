678dd5951feed8d7317c1155aeff5548b269c3b0
Jason Uwaeze <34732790+Wazhee@users.noreply.github.com>
2024-01-31 15:56:28 -0600

    Update BrownianBridgeModel.py

--- model/BrownianBridge/BrownianBridgeModel.py
+++ model/BrownianBridge/BrownianBridgeModel.py
@@ -131,7 +131,7 @@ class BrownianBridgeModel(nn.Module):
         x_t, objective = self.q_sample(x0, y, t, noise)
         objective_recon = self.denoise_fn(x_t, timesteps=t, context=context)
         BoundaryLoss = SurfaceLoss
-
+        boundary_loss = BoundaryLoss(idc=[1])
         if self.loss_type == 'l1':
             recloss = (objective - objective_recon).abs().mean()
             print(objective_recon.shape, mask.shape)
