0c0ae902bdf1b238761313d52c910cd15aca34cd
Jason Uwaeze <34732790+Wazhee@users.noreply.github.com>
2024-02-01 11:32:33 -0600

    Update utils.py

--- runners/utils.py
+++ runners/utils.py
@@ -61,9 +61,6 @@ def get_dataset(data_config):
     train_dataset = Registers.datasets[data_config.dataset_type](data_config.dataset_config, stage='train')
     val_dataset = Registers.datasets[data_config.dataset_type](data_config.dataset_config, stage='val')
     test_dataset = Registers.datasets[data_config.dataset_type](data_config.dataset_config, stage='test')
-    print('\n\n')
-    print(type(train_dataset))
-    print(len(train_dataset))
     return train_dataset, val_dataset, test_dataset
 
 
