8671ad7d9635b7f3ac15e7ecae506a07c00b99dc
Jason Uwaeze <34732790+Wazhee@users.noreply.github.com>
2024-02-05 13:20:28 -0600

    Update base.py

--- datasets/base.py
+++ datasets/base.py
@@ -32,14 +32,19 @@ class ImagePathDataset(Dataset):
 
         img_path = self.image_paths[index]
         image = None
-        try:
-            if(img_path.split('.')[1] == 'dcm'): # run dicom code
-                image = dicom.dcmread(img_path).pixel_array
-            else:
-                image = Image.open(img_path)
-            print(image.shape)
-        except BaseException as e:
-            print(e, img_path)
+        if(img_path.split('.')[1] == 'dcm'): # run dicom code
+                image = dicom.dcmread(image_path).pixel_array
+        else:
+            image = Image.open(img_path)
+        
+        # try:
+        #     if(img_path.split('.')[1] == 'dcm'): # run dicom code
+        #         image = dicom.dcmread(img_path).pixel_array
+        #     else:
+        #         image = Image.open(img_path)
+        #     print(image.shape)
+        # except BaseException as e:
+        #     print(e, img_path)
 
         # if not image.mode == 'RGB':
         #     image = image.convert('RGB')
